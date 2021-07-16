cb758b84f498df78af3c10b48a4b6a611db32053
shreyanshp25 <shreyansh2511@gmail.com>
2021-07-17 00:47:00 +0530

    added putMapping

--- src/main/java/com/Customer/Service/CustomerService.java
+++ src/main/java/com/Customer/Service/CustomerService.java
@@ -22,9 +22,20 @@ public class CustomerService {
 	public Customers postCust(Customers customer) {
 		return customerRepo.save(customer);
 	}
+	//get by id
 	public ResponseEntity<Customers> getById(Long id) {
 		Customers customer = customerRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Customer doesn't exist"+ id));
 		return ResponseEntity.ok(customer);
+	}
+	//update an entry
+	public ResponseEntity<Customers> updateCustomer(Long id, Customers customer) {
+		Customers cust = customerRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Customer to be updated doesn't exist"+ id));
+		cust.setFirstName(customer.getFirstName());
+		cust.setLastName(customer.getLastName());
+		cust.setEmailId(customer.getEmailId());
+		
+		Customers updatedCustomer = customerRepo.save(cust);
+		return ResponseEntity.ok(updatedCustomer);
 	}
 
 }
